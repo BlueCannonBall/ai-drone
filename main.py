5141e6ac670a18f4ec1e099a0fddee1a75991c76
250100 <250100@bps121.org>
2025-04-08 12:32:43 -0400

    Try to implement video recording

--- main.py
+++ main.py
@@ -1,12 +1,36 @@
+import time, cv2
+from threading import Thread
 from djitellopy import Tello
 
 tello = Tello()
 
 tello.connect()
+
+keepRecording = True
+tello.streamon()
+frame_read = tello.get_frame_read()
+
+def videoRecorder():
+    height, width, _ = frame_read.frame.shape
+    video = cv2.VideoWriter('video.avi', cv2.VideoWriter_fourcc(*'XVID'), 30, (width, height))
+
+    while keepRecording:
+        video.write(frame_read.frame)
+        time.sleep(1 / 30)
+
+    video.release()
+
+
+recorder = Thread(target=videoRecorder)
+recorder.start()
+
 tello.takeoff()
 
-tello.move_left(100)
-tello.rotate_counter_clockwise(90)
-tello.move_forward(100)
+tello.move_forward(200)
+tello.rotate_counter_clockwise(180)
+tello.move_forward(200)
 
 tello.land()
+
+keepRecording = False
+recorder.join()
