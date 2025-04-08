5be29a9bdf93e1686ddb5dbaab6eaa86f24de8b6
Akil <akilpugalenthi@gmail.com>
2025-04-08 18:54:34 -0400

    getting the video working

--- main.py
+++ main.py
@@ -8,29 +8,18 @@ tello.connect()
 
 keepRecording = True
 tello.streamon()
-frame_read = tello.get_frame_read()
-
-def videoRecorder():
-    height, width, _ = frame_read.frame.shape
-    video = cv2.VideoWriter('video.avi', cv2.VideoWriter_fourcc(*'XVID'), 30, (width, height))
-
-    while keepRecording:
-        video.write(frame_read.frame)
-        time.sleep(1 / 30)
-
-    video.release()
-
-
-recorder = Thread(target=videoRecorder)
-recorder.start()
+frame_read = tello.get_frame_read()    
 
 tello.takeoff()
 
-tello.move_forward(200)
-tello.rotate_counter_clockwise(180)
-tello.move_forward(200)
+while keepRecording:
+    cv2.imshow('frame', frame_read.frame)
+    #video.write(frame_read.frame)
+    if cv2.waitKey(1) == ord('q'):
+        break
 
+tello.move_up(100)
+tello.rotate_counter_clockwise(360)
 tello.land()
 
 keepRecording = False
-recorder.join()
